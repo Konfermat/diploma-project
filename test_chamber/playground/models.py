ad62df599b557241473a6ea3875dfb30c3e48b5a
Konfermat <semyon.obuhov@gmail.com>
2026-04-10 17:13:46 +0700

    db in progres ahh

--- test_chamber/playground/models.py
+++ test_chamber/playground/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 #  (OneToOneField), 
 # one to many (ForeignKey) 
 # and many to many (ManyToManyField).
 
-class User(models.Model):
-    name = models.CharField(max_length=20)
+class User(AbstractUser):
+    name = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Test(models.Model):
@@ -14,39 +15,44 @@ class Test(models.Model):
     #many to many (User)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    difficulty  (Choices: Easy, Medium, Hard)
+    is_published 
 
 class Question(models.Model):
     #optional many to one (Test)
-    test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
     description = models.TextField(max_length=1000)
 
 class Option(models.Model):
     #many to one (Question)
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     description = models.TextField(max_length=1000)
     is_correct = models.BooleanField(default=False)
 
 class Result(models.Model):
     # many to one (User)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # many to one (Test)
-    test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
     score = models.IntegerField()
     completed_at = models.DateTimeField(auto_now_add=True)
 
-class UserAttemt(models.Model):
+class UserAttempt(models.Model):
     # many to one (User)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     # many to one (Test)
-    test_id = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
     score = models.IntegerField()
 
 class UserAnswer(models.Model):
     # many to one (Question)
-    attempt_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    attempt = models.ForeignKey(UserAttempt, on_delete=models.CASCADE)
     # many to one (Question)
-    question_id = 
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # many to one (Option)
-    option_id = 
+    option = 
 
 # ОШИБКИ ИСПРАВЬ    
+
+class Topic(models.Model):
+    pass
