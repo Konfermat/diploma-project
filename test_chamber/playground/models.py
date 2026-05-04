9737ec9fc4f7c461d00d8792784de292f0baca07
Konfermat <semyon.obuhov@gmail.com>
2026-05-04 17:01:34 +0700

    duged more info

--- test_chamber/playground/models.py
+++ test_chamber/playground/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+# Что если все это по пробовать упростить?
 class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
