541e7a94bdef2e1647658c7b4e9a3b47e8ffcece
Konfermat <semyon.obuhov@gmail.com>
2026-05-20 07:55:13 +0700

    practice

--- BugBytes-tasukete/drf_cource/api/models.py
+++ BugBytes-tasukete/drf_cource/api/models.py
@@ -2,24 +2,17 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     pass
 
-<<<<<<< HEAD
-=======
 
->>>>>>> d408294c427b438ecb945af90a33ed3f9e6670c8
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-<<<<<<< HEAD
-    stock = models.PositiveBigIntegerField()
-    image = models.ImageField(upload_to='products/', blank=True, null=True) 
-=======
     stock = models.PositiveIntegerField()
     image = models.ImageField(upload_to='products/', blank=True, null=True)
->>>>>>> d408294c427b438ecb945af90a33ed3f9e6670c8
 
     @property
     def in_stock(self):
@@ -28,10 +21,7 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
-<<<<<<< HEAD
-=======
 
->>>>>>> d408294c427b438ecb945af90a33ed3f9e6670c8
 class Order(models.Model):
     class StatusChoices(models.TextChoices):
         PENDING = 'Pending'
@@ -42,19 +32,6 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(
-<<<<<<< HEAD
-        max_length=10, 
-        choices=StatusChoices.choices,
-        default=StatusChoices.PENDING
-        )
-    products = models.ManyToManyField(Product, through="OrderItem", related_name='orders')
-
-    def __str__(self):
-        return f"Order {self.order_id} by {self.user.username}"
-    
-class OrderItem():
-    
-=======
         max_length=10,
         choices=StatusChoices.choices,
         default=StatusChoices.PENDING
@@ -77,4 +54,3 @@ class OrderItem(models.Model):
     
     def __str__(self):
         return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
->>>>>>> d408294c427b438ecb945af90a33ed3f9e6670c8
--- BugBytes-tasukete/drf_cource/drf_cource/wsgi.py
+++ BugBytes-tasukete/drf_cource/drf_cource/wsgi.py
@@ -4,7 +4,7 @@ WSGI config for drf_cource project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
