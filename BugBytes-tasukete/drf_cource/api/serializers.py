990a04b3e0e46e4792313d916c4c54979c5caa83
Семен Обухов <main.atoll.nsk\USERSMAIL\korp8>
2026-05-21 17:27:02 +0700

    tranfer

---
+++ BugBytes-tasukete/drf_cource/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Product, Order, OrderItem
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'description',
+            'price',
+            'stock',
+            'image',
+        ]
