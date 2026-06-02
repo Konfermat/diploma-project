52e21201b8b4c06571d36eca0c1af154eab42fae
Konfermat <semyon.obuhov@gmail.com>
2026-06-02 07:47:10 +0700

    transfer to j

--- test_chamber/api/managment/commands/populate_db copy.py
+++ test_chamber/api/managment/commands/populate_db copy.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
         # get or create superuser
         user = User.objects.filter(username='admin').first()
         if not user:
-            user = User.objects.create_superuser(username='admin', password='test')
+            user = User.objects.create_superuser(username='admin', password='1234')
 
         # create products - name, desc, price, stock, image
         products = [
--- test_chamber/api/serializers.py
+++ test_chamber/api/serializers.py
@@ -1,50 +1,13 @@
 from rest_framework import serializers
-from .models import Step, StepElement, TextElement, TestElement, TestOption
+from .models import Course
 
-# сделай сам
-
-# --- СЕРИАЛИЗАТОРЫ КОНТЕНТА ---
-
-class TextElementSerializer(serializers.ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
     class Meta:
-        model = TextElement
-        fields = ['body']
+        model = Course
+        fields = [
+            'id',
+            'title',
+            'description',
+            'created_at',
+        ]
 
-
-class TestOptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TestOption
-        fields = ['id', 'answer'] 
-
-
-class TestElementSerializer(serializers.ModelSerializer):
-    options = TestOptionSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = TestElement
-        fields = ['question', 'options']
-
-
-# --- СЕРИАЛИЗАТОРЫ СТРУКТУРЫ ---
-
-class StepElementSerializer(serializers.ModelSerializer):
-    content = serializers.SerializerMethodField()
-
-    class Meta:
-        model = StepElement
-        fields = ['id', 'order', 'step_element_type', 'content']
-
-    def get_content(self, obj):
-        if obj.step_element_type == 'TEXT' and hasattr(obj, 'text_content'):
-            return TextElementSerializer(obj.text_content).data
-        if obj.step_element_type == 'TEST' and hasattr(obj, 'test_content'):
-            return TestElementSerializer(obj.test_content).data
-        return None
-
-
-class StepDetailSerializer(serializers.ModelSerializer):
-    elements = StepElementSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Step
-        fields = ['id', 'title', 'order', 'elements']
