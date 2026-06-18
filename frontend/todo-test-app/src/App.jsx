7d9898bd630b302516f02b7d156fae8bfd118a24
Konfermat <semyon.obuhov@gmail.com>
2026-06-18 17:21:53 +0700

    fast push transfer

--- frontend/todo-test-app/src/App.jsx
+++ frontend/todo-test-app/src/App.jsx
@@ -1,34 +1,31 @@
-import Form from "./components/Form";
-import FilterButton from "./components/FilterButton";
-import Todo from "./components/Todo";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Todo from "./components/Todo.jsx"
+import Home from "./components/Home.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
+import { useState } from "react"
 
-function App(props) {
-  const taskList = props.tasks?.map((task) => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-    />
-  ));
+// Даже если это просто начальные данные, их лучше вынести в отдельный файл (например, src/data/todoData.js), чтобы не загромаждать App.jsx.
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+
+function App() {
+  const [tasks, setTasks] = useState(DATA);
+
   return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-        {taskList}
-      </ul>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={ <Home tasks={DATA} /> } /> */}
+        <Route path="/" element={ <Home tasks={tasks} setTasks={setTasks} /> } />
+        <Route path="/todo" element={ <Todo /> } />
+      </Routes>
+    </BrowserRouter>
+
+
   );
 }
 
---
+++ frontend/todo-test-app/src/components/Home.jsx
@@ -0,0 +1,34 @@
+import Form from "./Form";
+import FilterButton from "./FilterButton";
+import Todo from "./Todo";
+
+function Home(props) {
+  const taskList = props.tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ));
+  return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
